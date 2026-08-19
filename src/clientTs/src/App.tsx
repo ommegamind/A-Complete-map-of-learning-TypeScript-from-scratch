eb59c7c9cb6058121b041b580c9fe70a5533a721
Om Garg <omgarg3000@gmail.com>
2026-08-19 21:30:12 +0530

    learning react and hooks with ts

---
+++ src/clientTs/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { CardElement } from "./Card";
+
+function App() {
+  return (
+    <>
+      <h1>Hello omie</h1>
+      <CardElement prodName={"coffee"} prodPrice={100} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/clientTs/src/Card.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface ElementType {
+  prodName: string;
+  prodPrice: number;
+  isSpecial?: boolean;
+}
+
+export const CardElement = ({
+  prodName,
+  prodPrice,
+  isSpecial = false,
+}: ElementType) => {
+  return (
+    <>
+      <h2>Name: {prodName}</h2>
+      <h2>Price: {prodPrice}</h2>
+      <h2>Special: {isSpecial ? "yes" : "no"}</h2>
+      <Counter />
+    </>
+  );
+};
+
+export const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+  return (
+    <>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </>
+  );
+};
